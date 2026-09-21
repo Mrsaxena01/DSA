b9b920604ef521b832515cb237dbcb9a580a1e6c
Mrsaxena01 <sonusaxena7667@gmail.com>
2026-09-21 16:18:26 +0530

    Central pyramid

---
+++ Day-02/centralPyramid.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Enter a number : ";
+    cin >> num;
+
+    for (int row = 1; row <= num; row++)
+    {
+        // space
+        for (int col = 1; col <= num - row; col++)
+        {
+            cout << " ";
+        }
+
+        // star
+        for (int col = 1; col <= 2 * row - 1; col++)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
